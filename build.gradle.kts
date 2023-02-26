39acffada0f10837205c0e3b7b35f9a941487370
Etjen Delilovic <etjen.delilovic@trx-base.com>
2023-02-26 19:36:10 +0100

    Updated publish directory for Netlify.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,6 @@ plugins {
     id("io.github.fstaudt.hugo") version "0.5.1"
 }
 
-tasks.hugoBuild {
-    doFirst {
-        exec {
-            workingDir("site/themes/tailwind")
-            commandLine("npm", "install")
-        }
-    }
-}
-
 tasks.hugoServer {
     doFirst {
         exec {
